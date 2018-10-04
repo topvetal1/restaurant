cfd4041243d1e1f54d5845926f118e7056e3d641
Виталий <32413174+topvetal1@users.noreply.github.com>
2018-10-04 14:32:17 +0300

    page
    Добавил кухню

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
-    var itemFlip = document.querySelectorAll('.menu__item.animate');
+    var itemFlip = document.querySelectorAll('.menu_item');
     //Анимация бургера
-    $('.menu__button').click(function() {
+    $('.menu_button').click(function() {
         $(this).toggleClass('open');
         //Анимация итемов меню
         if ($(this).attr('data-click') == 0) {
             $(this).attr('data-click', 1);
-            setTimeout(function() {
-                Array.from(itemFlip).forEach(async (item, i) => {
-                    setTimeout(() => item.classList.add('animate'), i * 100);
-                });
-            }, 100);
+            $('.menu_item').each(function(i) {
+                var self = $(this);
+                setTimeout(function () {
+                    self.hide(200);
+                }, i * 100);
+            });
         } else {
             $(this).attr('data-click', 0);
-            setTimeout(function() {
-                Array.from(itemFlip).forEach((item, i) => {
-                    setTimeout(() => item.classList.remove('animate'), i * 100);
-                });
-            }, 100);
+            $('.menu_item').each(function(i) {
+                var self = $(this);
+                console.log(self);
+                setTimeout(function () {
+                    self.show(200);
+                }, i * 100);
+            });
         }
     });
 })
