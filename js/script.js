f3aea36d338f7ff2b6c5847cc22027df1bbe2950
Виталий <32413174+topvetal1@users.noreply.github.com>
2018-10-05 13:37:27 +0300

    nav

--- js/script.js
+++ js/script.js
@@ -23,4 +23,30 @@ $(document).ready(function() {
             });
         }
     });
+    $('.item a').on('click', function(e) {
+        e.preventDefault();
+        var _this = $(this),
+            item = _this.closest('.item'),
+            direction = _this.attr('href').replace('#', ''),
+            reqSection = $('section').filter('[data-target="' + direction + '"]'),
+            reqSecPos = reqSection.offset().top;
+        $('html, body').animate({
+            scrollTop: reqSecPos
+        }, 500);
+    })
+    $(window).on('scroll', function() {
+        $('section').each(function(){
+            var _this = $(this),
+                topSection = _this.offset().top - 50,
+                botSection = topSection + _this.height(),
+                wScroll = $(window).scrollTop();
+            if(topSection < wScroll && botSection > wScroll) {
+                var id = _this.index(),
+                    stroka = _this.attr('data-target'),
+                    reqItem = $('.item a').eq(id);
+                reqItem.closest('.item').addClass('active').siblings().removeClass('active');
+                window.location.hash = stroka;
+            }
+        })
+    })
 })
