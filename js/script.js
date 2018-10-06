84c8712c35d8cb925f5fa9a8550ce7808c30ea9a
Виталий <32413174+topvetal1@users.noreply.github.com>
2018-10-07 01:57:28 +0300

    top menu and burger

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     var itemFlip = document.querySelectorAll('.menu_item');
+    var topItem = $('top_memu a');
     //Анимация бургера
     $('.menu_button').click(function() {
         $(this).toggleClass('open');
-        //Анимация итемов меню
+        //Анимация итемов меню бургера
         if ($(this).attr('data-click') == 0) {
             $(this).attr('data-click', 1);
             $('.menu_item').each(function(i) {
@@ -23,30 +24,32 @@ $(document).ready(function() {
             });
         }
     });
+    //Click and animation top menu
     $('.item a').on('click', function(e) {
         e.preventDefault();
         var _this = $(this),
             item = _this.closest('.item'),
             direction = _this.attr('href').replace('#', ''),
-            reqSection = $('section').filter('[data-target="' + direction + '"]'),
+            reqSection = $('.all').filter('[data-target="' + direction + '"]'),
             reqSecPos = reqSection.offset().top;
         $('html, body').animate({
             scrollTop: reqSecPos
         }, 500);
     })
     $(window).on('scroll', function() {
-        $('section').each(function(){
+        $('.all').each(function(){
             var _this = $(this),
-                topSection = _this.offset().top - 50,
+                topSection = _this.offset().top,
                 botSection = topSection + _this.height(),
-                wScroll = $(window).scrollTop();
-            if(topSection < wScroll && botSection > wScroll) {
+                wScroll = $(window).scrollTop(),
+                wHeight = $(window).height()/2
+            if((topSection - wHeight < wScroll) && (botSection - wHeight > wScroll)) {
                 var id = _this.index(),
                     stroka = _this.attr('data-target'),
                     reqItem = $('.item a').eq(id);
                 reqItem.closest('.item').addClass('active').siblings().removeClass('active');
                 window.location.hash = stroka;
             }
-        })
-    })
+        });
+    });
 })
